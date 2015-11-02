f2c51a8d7e7c52b20277d579c5ea95550a1ce684
Kris Sankaran <sankaran.kris@gmail.com>
2015-11-02 10:54:08 -0800

    Update bernoulli experiment with 2-d example

--- vignettes/bernoulli_experiment.R
+++ vignettes/bernoulli_experiment.R
@@ -8,30 +8,26 @@ library("ggplot2")
 library("reshape2")
 library("plyr")
 library("dplyr")
+library("expPCA")
+theme_set(theme_bw())
 
 ## ---- setup ----
 n <- 1000
-k <- 3
+k <- 5
 d <- 15
-iter_max <- 50
 X_data <- generate_data(k, n, d, .5, 0.1)
 heatmap(X_data$X)
 
 ## ---- pca ----
-pca_res <- bern_exp_pca(X_data$X, iter_max = iter_max, eps = 1e-10, lambda = 0.05)
-a_df <- data.frame(a = pca_res$a, label = X_data$copies)
+pca_res <- bern_exp_pca(X_data$X, 2, 5, 10)
+a_df <- data.frame(a = pca_res$A, label = X_data$copies)
+head(a_df)
 
 ## ---- study-pca ----
 ggplot(a_df) +
-  geom_jitter(aes(x = a, y = 0, col = as.factor(label)), alpha = 0.5) +
+  geom_jitter(aes(x = a.1, y = a.2, col = as.factor(label)), alpha = 0.5) +
   ylim(c(-5, 5)) +
-  ggtitle("Exponential Family PCA Scores (1-d)")
-
-ggplot(a_df) +
-  geom_jitter(aes(x = a, y = 0, col = as.factor(label))) +
-  ylim(c(-5, 5)) +
-  facet_wrap(~label) +
-  ggtitle("Exponential Family PCA Scores (1-d)")
+  ggtitle("Exponential Family PCA Scores (2-d)")
 
 Z <- data.frame(X_data$X, label = X_data$copies)
 Z$ix <- 1:nrow(Z)
